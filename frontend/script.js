c722dd276460c82b8e512f84932d26302e5f2344
Siddharth <siddharths51700@gmail.com>
2018-10-30 23:23:37 +0530

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,18 @@
+let socket= io();
+socket.on('connected',()=>{
+    console.log("Connected "+socket.id);
+})
+
+$(function(){
+    let msglist=$('#msglist')
+    let sendbtn=$('#sendmsg')
+    let msgbox=$('#msgbox')
+
+    sendbtn.click(function(){
+        socket.emit('send_msg', {message: msgbox.val()})
+    })
+
+    socket.on('recv_msg',function(data){
+        msglist.append($('<li>'+ data.message+ '</li>'))
+    })
+})
---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const path=require('path')
+const socketio=require('socket.io')
+const http=require('http')
+const app=express();
+const server=http.createServer(app);
+
+const io=socketio(server)
+app.use('/',express.static(path.join(__dirname,'frontend')));
+
+io.on('connection',(socket)=>{
+    console.log("New socket formed from "+socket.id)
+    socket.emit('connected')
+
+    
+    socket.on('send_msg',(data)=>{
+        //console.log("Recieved Message = " + data.message)
+        io.emit('recv_msg', data);
+    })
+})
+
+
+server.listen(1478);
